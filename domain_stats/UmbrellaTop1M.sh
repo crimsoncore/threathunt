d8e9561b4cf73ad5a89d8c6bcdaa2fbeaa376e9d
luks_mbp <luks@crimsoncore.be>
2020-04-01 12:15:56 +0200

    added docker restart after update top1m sites

--- domain_stats/UmbrellaTop1M.sh
+++ domain_stats/UmbrellaTop1M.sh
@@ -1,2 +1,3 @@
 wget -O Umbrella-Top-1m.zip http://s3-us-west-1.amazonaws.com/umbrella-static/top-1m.csv.zip
-unzip Umbrella-Top-1m.zip 
+unzip -o Umbrella-Top-1m.zip
+docker container restart domainstats
