2fe823dcf5212fedab0ff946d491413239f31864
pblaton <pieterjanblaton@gmail.com>
2020-10-01 16:27:20 +0000

    change gpo and add netlogon

--- labsetup/DC/BGInfo/bginfo.bat
+++
@@ -1,5 +0,0 @@
-timeout /t 10 /nobreak
-
-reg add HKU\.DEFAULT\Software\Sysinternals\BGInfo /v EulaAccepted /t REG_DWORD /d 1 /f
-
-\\bigteclab.be\NETLOGON\Bginfo\Bginfo.exe \\bigteclab.be\NETLOGON\Bginfo\bigteclab.bgi /TIMER:00 /nolicprompt
