e0f1eb2d2d1bfe6b0de43e1172b3e8b3b8f22b73
pblaton <pieterjanblaton@gmail.com>
2022-04-04 16:17:40 +0000

    compose rabbitmq script

--- docker-compose/compose_RabbitMQ.sh
+++ docker-compose/compose_RabbitMQ.sh
@@ -1,1 +1,1 @@
-docker-compose -f docker-compose.rabbitmq.yml -f docker-compose.logstashMQ.yml -f docker-compose.portainer.yml -f docker-compose.guacamole.yml -f up -d
+docker-compose -f docker-compose.traefik.yml -f docker-compose.portainer.yml -f docker-compose.guacamole.yml -f docker-compose.rabbitmq.yml -f docker-compose.logstashMQ.yml -f docker-compose.mdbook.yml up -d
